5e01ef099d82d197462648910a0f92a7bdb9ec9f
Nabil <Nabil@gmail.com>
2026-01-14 05:43:32 +0100

    Wed Jan 14 05:43:32 AM CET 2026

--- 00_MySQL_NodeJS/notes/Day_1/app.js
+++ 00_MySQL_NodeJS/notes/Day_1/app.js
@@ -1,25 +1,22 @@
 import mysql from "mysql2";
-
 const connection = mysql.createConnection({
   host: "localhost",
   user: "admin",
   password: "your_password",
   database: "arch_study",
 });
-
 connection.connect((err) => {
   if (err) {
-    console.error("ERROR --> ", err.stack);
+    console.error("ERROR: Connection --> " + err.stack);
     return;
   }
-  console.log("OK --> ", connection.threadId);
+  console.log("OK: Connection --> " + connection.threadId);
 });
 
 const sql = "INSERT INTO learners (name, topic) VALUES (?, ?)";
-const values = ["Your Name", "MySQL & Node.js Day 1"];
-
+const values = ["Ayman", "Learn HTML"];
 connection.query(sql, values, (err, result) => {
   if (err) throw err;
-  console.log("OK --> Add user data: " + result.insertId);
+  console.log("OK: Add user --> " + result.insertId);
   connection.end();
 });
