82582859f3431306c4287e7e6aed1267e5db251c
Nabil <Nabil@gmail.com>
2026-01-16 12:22:51 +0100

    0

--- 99_More/HTTPie.sh
+++ 99_More/HTTPie.sh
@@ -7,3 +7,16 @@ pip install --upgrade pip
 pip install httpie
 http --version
 deactivate
+
+# How to use HTTPie
+cd ~
+source venv/bin/activate
+# Normal url ==========================================
+http localhost:3000/api/user
+http localhost:3000/api/user/1
+http delete localhost:3000/api/user/1
+# Json Body ===========================================
+http post localhost:3000/api/user   name=Nabil age:=10
+http put  localhost:3000/api/user/1 name=Nabil job:=true
+# Querys ==============================================
+http localhost:3000/api/user name==Nabil age==10
